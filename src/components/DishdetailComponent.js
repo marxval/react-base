dcfbfa2b01d909db362eab016c5fa4e8ec590c0c
mxmghost <mxmghost@gmail.com>
2020-07-13 13:23:58 -0500

    Add header and footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 function renderComments(comments) {
   if (comments != null)
